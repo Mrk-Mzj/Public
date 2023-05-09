c8feb721fc9c0921ccea284cd86ee01a9f3e7195
Mrk-Mzj <Marek.Mazij@gmail.com>
2023-05-09 12:18:38 +0200

    first commit: TODO list

---
+++ password_manager/pass.py
@@ -0,0 +1,7 @@
+# TODO: mySQL database create / open
+# TODO: view, add, modify, delete entry (CRUD operations)
+# TODO: view as a formatted text table
+# TODO: search
+# TODO: encryption
+# TODO: login, password, change password
+# TODO: interface
