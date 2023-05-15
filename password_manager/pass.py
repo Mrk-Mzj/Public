1b0e8e194dc733b410f6d9dc7e460d68d7caafc9
Mrk-Mzj <Marek.Mazij@gmail.com>
2023-05-15 15:05:14 +0200

    - enabled: loging in with username other than 'root' - refactored: code splitted into more convinient functions

--- password_manager/pass.py
+++ password_manager/pass.py
@@ -6,66 +6,79 @@ python -m pip install mysql-connector-python
 
 https://www.w3schools.com/python/python_mysql_getstarted.asp
 """
+# login, pass: check .gitignore
 
-# python password_manager/pass.py PASSWORD
+
+# Standard library imports:
+import sys
+
+# Third party imports:
+import mysql.connector
+from mysql.connector import errors
 
 
 def main():
-    # Standard library imports:
-    import sys
-
-    # Third party imports:
-    import mysql.connector
-    from mysql.connector import errors
 
     # password chcecking:
-    MY_SQL_PASSWORD = password_check()
-
-    print()
+    MY_SQL_USER, MY_SQL_PASSWORD = login_db()
 
     # connecting to the database:
-    try:
-        mydb = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password=MY_SQL_PASSWORD,
-            database="db_pass_python",  # TODO: check if there is db_pass_python database. If not: create_database(mycursor, db_pass_python)
-        )
-        mycursor = mydb.cursor()
+    mydb = connect_db(MY_SQL_USER, MY_SQL_PASSWORD)
+    mycursor = mydb.cursor()
 
-    except errors.ProgrammingError as e:
-        sys.exit(f"cannot connect to MySQL database: \n{e}")
+    # show all databases:
+    show_all_db(mycursor)
 
-    # show databases:
-    mycursor.execute("SHOW DATABASES")
-    for x in mycursor:
-        print(x)
+    # close the cursor and the connection
+    mycursor.close()
+    mydb.close()
+
 
     # TODO: view, add, modify, delete entry (CRUD operations)
     # TODO: view as a formatted text table
     # TODO: search
     # TODO: encryption
-    # TODO: login, password, change password
     # TODO: interface
 
 
-    # Close the cursor and the connection
-    mycursor.close()
-    mydb.close()
-
-
 # chcecking if program was launched with password as parameter:
-def password_check():
-    import sys
-    if len(sys.argv) != 2:
-        sys.exit("\nYou must run program with mySQL password as a parameter")
+def login_db():
+
+    print()
+    if len(sys.argv) != 3:
+        sys.exit(
+            "You must add mySQL username and password as parameters: 'python pass.py username password'"
+        )
     else:
-        return sys.argv[1]
+        return sys.argv[1], sys.argv[2]
+
+
+# connecting to the database:
+def connect_db(MY_SQL_USER, MY_SQL_PASSWORD):
+    try:
+        mydb = mysql.connector.connect(
+            host = "localhost",
+            user = MY_SQL_USER,
+            password = MY_SQL_PASSWORD,
+            database = "db_pass_python",  # TODO: check if there is db_pass_python database. If not: create_database(mycursor, db_pass_python)
+        )
+        return mydb
+
+    except errors.ProgrammingError as e:
+        sys.exit(f"cannot connect to MySQL database: \n{e}")
+
 
 # create database:
 def create_database(mycursor, name="db_pass_python"):
     mycursor.execute("CREATE DATABASE {name}")
 
 
+# show all databases:
+def show_all_db(mycursor):
+    mycursor.execute("SHOW DATABASES")
+    for x in mycursor:
+        print(x)
+
+        
 if __name__ == "__main__":
     main()
