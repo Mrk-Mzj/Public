9f2e1054428af96cdcf77d5f0913cb433960cade
Mrk-Mzj <Marek.Mazij@gmail.com>
2023-10-15 00:59:24 +0200

    refactor: function now uses STR as input, which allows you to check numbers starting with 0

--- other/pesel_check/is_polish_national_id.py
+++ other/pesel_check/is_polish_national_id.py
@@ -1,27 +1,42 @@
-def is_polish_national_id(input_int: int) -> bool:
+def is_polish_national_id(input_str: str) -> bool:
     """
     Verification of the correctness of the PESEL number.
     www-gov-pl.translate.goog/web/gov/czym-jest-numer-pesel?_x_tr_sl=auto&_x_tr_tl=en
 
+    PESEL can start with 0, that's why we take str as input,
+    but convert it to int for some calculations.
 
-    >>> is_polish_national_id(02070803628)
+
+    >>> is_polish_national_id("02070803628")
     True
 
-    >>> is_polish_national_id(-99012212349)
+    >>> is_polish_national_id("-99012212349")
     False
 
-    >>> is_polish_national_id(990122123499999)
+    >>> is_polish_national_id("990122123499999")
     False
 
-    >>> is_polish_national_id("abc!@#")
+    >>> is_polish_national_id(123)
     Traceback (most recent call last):
         ...
-    ValueError: Expected int as input, found <class 'str'>
+    ValueError: Expected str as input, found <class 'int'>
+
+    >>> is_polish_national_id("abc")
+    Traceback (most recent call last):
+        ...
+    ValueError: Expected number as input
     """
 
     # check for invalid input type
-    if not isinstance(input_int, int):
-        msg = f"Expected int as input, found {type(input_int)}"
+    if not isinstance(input_str, str):
+        msg = f"Expected str as input, found {type(input_str)}"
+        raise ValueError(msg)
+
+    # check if input can be converted to int
+    try:
+        input_int = int(input_str)
+    except ValueError:
+        msg = "Expected number as input"
         raise ValueError(msg)
 
     # check number range (00010100000-99923199999)
@@ -35,7 +50,7 @@ def is_polish_national_id(input_int: int) -> bool:
     multipliers = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
     sum = 0
 
-    digits_to_check = str(input_int)[:-1]  # cut off the checksum
+    digits_to_check = str(input_str)[:-1]  # cut off the checksum
 
     for index, digit in enumerate(digits_to_check):
         # Multiply corresponding digits and multipiers
